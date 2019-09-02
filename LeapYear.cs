fbb78f563bd791224ac41bb8f4eb4f658959f847
nikhiljainlive <47408125+nikhiljainlive@users.noreply.github.com>
2019-09-02 18:01:38 +0530

    Update LeapYear.cs

--- LeapYear.cs
+++ LeapYear.cs
@@ -2,7 +2,7 @@
 // <copyright file="LeapYear.cs" company="Bridgelabz">
 //   Copyright © 2018 Company
 // </copyright>
-// <creator name="Rahul Gajare"/>
+// <creator name="Nikhil Jain"/>
 // ---------------------------------------------------------------------------------------------------------
 
 namespace FunctionalPrograms
